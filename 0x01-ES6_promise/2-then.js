c436a0961c3b351440ce0abbff8b233244960311
root <hirwawilly9@gmail.com>
2024-03-07 00:31:32 +0000

    Saving my files

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    return { status: 200, body: 'success' };
-  }).catch((error) => {
-    return new Error();
-  }).finally(() => {
-    console.log('Got a response from the API');
-  });
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch((error) => {
+      console.error(error);
+      throw new Error();
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
