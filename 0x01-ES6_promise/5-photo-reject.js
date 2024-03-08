5dd0cb3a10eee5b05db5fe262f65d1a4a09bae86
root <hirwawilly9@gmail.com>
2024-03-08 14:17:30 +0000

    Saving file 5

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(fileName) {
-  return new Promise((reject) => {
+  return Promise((reject) => {
     reject(new Error(`${fileName} cannot be processed`));
   });
 }
