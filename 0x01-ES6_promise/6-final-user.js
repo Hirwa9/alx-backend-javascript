7f28a43815f6a63eb8e46504375bffc83bda9223
root <hirwawilly9@gmail.com>
2024-03-09 10:32:36 +0000

    Saving file 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,12 @@
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = await signUpUser(firstName, lastName)
-    .then((response) => ({ status: response }));
-  const photoPromise = await uploadPhoto(fileName)
-    .catch((error) => ({ status: error }));
-  return [userPromise, photoPromise];
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: 'success', value: result.value };
+      }
+      return { status: 'error', value: result.reason };
+    }));
 }
