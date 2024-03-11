f1cbc9d89d620d9c5c02af268068c565bde1f169
root <hirwawilly9@gmail.com>
2024-03-11 01:27:52 +0000

    Saving file 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
-async function handleProfileSignup() {
+export default function handleProfileSignup() {
   try {
-    const [photoUploadRes, createUserRes] = await Promise.all([uploadPhoto, createUser]);
+    const [photoUploadRes, createUserRes] = Promise.all([uploadPhoto, createUser]);
     console.log(`${photoUploadRes.body} ${createUserRes.firstName} ${createUserRes.lastName}`);
   } catch (error) {
     console.error('Signup system offline');
