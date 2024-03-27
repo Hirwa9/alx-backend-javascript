e4850647f5944fd66e0ea43ad8f1f4819eb3c87b
root <hirwawilly9@gmail.com>
2024-03-27 13:00:36 +0000

    Saving file 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,13 @@
 export default function cleanSet(set, startString) {
+  const arrFromSet = Array.from(set);
   const foundStrArray = [];
   const startStringLen = startString.length;
   if (startStringLen < 1) {
     return '';
   }
   let foundStr = '';
-  set.forEach((element) => {
+  console.log(arrFromSet);
+  arrFromSet.forEach((element) => {
     if (element.startsWith(startString)) {
       const toAppdend = element.slice(startStringLen);
       foundStrArray.push(toAppdend);
